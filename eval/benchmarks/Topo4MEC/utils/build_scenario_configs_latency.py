84e8a5948f6f6464d3a160a3c2e2928d7950b929
Zaixing Sun <zxa2ng.sun@gmail.com>
2025-04-02 20:24:49 +0800

    update 22 files

--- eval/benchmarks/Topo4MEC/utils/build_scenario_configs_latency.py
+++ eval/benchmarks/Topo4MEC/utils/build_scenario_configs_latency.py
@@ -2,28 +2,31 @@ import os
 import json
 import random
 
-
-def main():
-    # 1. loading source files
-    flag, n = '25N50E', 25
-    # flag, n = '50N50E', 50
-    # flag, n = '100N150E', 100
-    # flag, n = 'MilanCityCenter', 30
-    with open(f"eval/benchmarks/Topo4MEC/source/{flag}/graph.txt", 'r') as f:
+def generate_configs(scenario, nodes_num):
+    """
+    Generate configuration files for the given scenario and number of nodes.
+    :param scenario: Scenario name (e.g., '25N50E', '50N50E', '100N150E', 'MilanCityCenter').
+    :param nodes_num: Number of nodes in the scenario.
+    :return: None
+    """
+   
+    # Load the graph data from the specified scenario file.
+    with open(f"eval/benchmarks/Topo4MEC/source/{scenario}/graph.txt", 'r') as f:
         graph_lines = f.readlines()
         edges_lines = [line.split() for line in graph_lines]
         edges_lines = [[int(line[0]) - 1, int(line[1]) - 1, float(line[2])]
                        for line in edges_lines]  # RayCloudSim is 0-index
-    save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/config.json"
 
-    # 2. nodes & edges
-    # keys = ['NodeType', 'NodeName', 'NodeId', 'MaxCpuFreq', 'MaxBufferSize', 
-    #         'IdleEnergyCoef', 'ExeEnergyCoef', ]
+    # Create a directory to save the configuration files.
+    save_as = f"eval/benchmarks/Topo4MEC/data/{scenario}/config.json"
+
+
+    # Generate nodes and edges configurations.
     nodes = []
-    for node_id in range(n):
-
+    for node_id in range(nodes_num):
         idle_energy_coef = max(0.01 * random.random(), 0.001)
         exe_energy_coef = 10 * idle_energy_coef
+        param_Latency = (0, 0.005)  # s
 
         nodes.append(
             {
@@ -34,9 +37,10 @@ def main():
                 'MaxBufferSize': random.randint(5, 40) * 10,  # Mb
                 'IdleEnergyCoef': round(idle_energy_coef, 4),
                 'ExeEnergyCoef': round(exe_energy_coef, 4),
+                'Latency': round(random.uniform(*param_Latency), 4),  # s
             }
         )
-    # keys = ['EdgeType', 'SrcNodeID', 'DstNodeID', 'Bandwidth']
+
     edges = []
     for src, dst, bw in edges_lines:
         edges.append(
@@ -47,26 +51,44 @@ def main():
                 'Bandwidth': 10 * bw,  # Mbps
             }
         )
-    
-    # 3. saving
+
+    # Save the generated configurations to a JSON file.
     data = {
         'Nodes': nodes,
         'Edges': edges,
     }
     json_object = json.dumps(data, indent=4)
-    if not os.path.exists(save_as):
-        with open(save_as, 'w+') as fw:
+    
+    with open(save_as, 'w+') as fw:
             fw.write(json_object)
-    else:
-        print("File already exists!")
+    # if not os.path.exists(save_as):
+    #     with open(save_as, 'w+') as fw:
+    #         fw.write(json_object)
+    # else:
+    #     print(f"File {save_as} already exists!")
 
-    # 4. loading
+    # Load the configurations from the JSON file.
     with open(save_as, 'r') as fr:
         json_object = json.load(fr)
         nodes, edges = json_object['Nodes'], json_object['Edges']
-
     print(f"{len(nodes)} nodes, {len(edges)} edges")
+    
+
+
+
+
+def main():
+    # 1. loading source files
+    scenarios = ['25N50E', '50N50E', '100N150E', 'MilanCityCenter']
+    nodes_nums = [25, 50, 100, 30]
+
+    # 2. generate configs
+    for scenario, nodes_num in zip(scenarios, nodes_nums):
+        generate_configs(scenario, nodes_num)
+    print("Finished generating configuration files.")
+
 
 
 if __name__ == '__main__':
+    random.seed(878289)
     main()
--- eval/benchmarks/Topo4MEC/utils/build_trainset-testset.py
+++ eval/benchmarks/Topo4MEC/utils/build_trainset-testset.py
@@ -4,80 +4,225 @@ import numpy as np
 import pandas as pd
 
 
-def main():
-    # 1. loading source files
-    flag, num_arrivals_per_ingress, lambda_, ingress_line = '25N50E', 5000, 0.04, list(range(25))
-    # flag, num_arrivals_per_ingress, lambda_, ingress_line = '50N50E', 2500, 0.02, list(range(50))
-    # flag, num_arrivals_per_ingress, lambda_, ingress_line = '100N150E', 1250, 0.01, list(range(100))
-    # flag, num_arrivals_per_ingress, lambda_, ingress_line = 'MilanCityCenter', 5000, 0.03333, list(range(30))
-
-    save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/trainset.csv"
-    params_save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/trainset_configs.json"
-
-    # 2. parameters
-    header = ['TaskName', 'GenerationTime', 'TaskID', 'TaskSize', 'CyclesPerBit', 
-              'TransBitRate', 'DDL', 'SrcName']  # field names
-    lambdas = [lambda_] * len(ingress_line)  # Average rate of arrivals per time unit for each ingress node
-    param_TaskSize = (10, 100 + 1)  # Mb
-    param_CyclesPerBit = (100, 1000 + 1)  # per-MBit
-    param_TransBitRate = (1, 5)  # Mbps
-    param_DDL = (20, 100 + 1)  # s
-
-    # 3. synthetic tasks
-    tasks = []
-    for node_id, lambda_ in zip(ingress_line, lambdas):
-        # Generate inter-arrival times
-        inter_arrival_times = np.random.exponential(1 / lambda_, num_arrivals_per_ingress)
-        # Compute arrival times
-        arrival_times = np.cumsum(inter_arrival_times)
-        arrival_times = np.round(arrival_times).tolist()
-
-        for t in arrival_times:
-            tasks.append(
-                [
-                    't0', # TaskName (invalid)
-                    t, # GenerationTime
-                    0,  # TaskID (invalid)
-                    np.random.randint(*param_TaskSize),  # TaskSize
-                    np.random.randint(*param_CyclesPerBit),  # CyclesPerBit
-                    10 * np.random.randint(*param_TransBitRate),  # TransBitRate
-                    np.random.randint(*param_DDL),  # DDL
-                    f'n{node_id}',  # SrcName
-                ]
-            )
+def generate_tasks(num_arrivals_per_ingress, lambda_, ingress_line, param_TaskSize, param_CyclesPerBit, param_TransBitRate, param_DDL):
+    """
+    Generate synthetic tasks based on the given parameters.
     
+    :param num_arrivals_per_ingress: Number of arrivals per ingress node
+    :param lambda_: Average rate of arrivals per time unit for each ingress node
+    :param ingress_line: List of ingress node IDs
+    :param param_TaskSize: Tuple of min and max task size
+    :param param_CyclesPerBit: Tuple of min and max cycles per bit
+    :param param_TransBitRate: Tuple of min and max transmission bit rate           
+    :param param_DDL: Tuple of min and max deadline
+    # :papram param_Latency: Tuple of min and max latency
+    :return: List of generated tasks
+    """
+    tasks = []
+    lambdas = [lambda_] * len(ingress_line)  # Average rate of arrivals per time unit for each ingress node
+    for node_id, lambda_ in zip(ingress_line, lambdas):
+        # # Generate inter-arrival times
+        # inter_arrival_times = np.random.exponential(1 / lambda_, num_arrivals_per_ingress)
+        # # Compute arrival times
+        # arrival_times = np.cumsum(inter_arrival_times)
+        # arrival_times = np.round(arrival_times).tolist()
+
+        num_arrivals_0 = list(np.random.poisson(lambda_, num_arrivals_per_ingress))
+        num_arrivals = []
+        k = 0
+        while k<num_arrivals_per_ingress:
+            k1 = num_arrivals_0.pop(0)+1
+            num_arrivals.append(k1)
+            k += k1
+        sum1 = sum(num_arrivals)
+        if sum1>num_arrivals_per_ingress:
+            num_arrivals[-1] -= (sum1 - num_arrivals_per_ingress)
+        interarrival_times = np.round( np.random.exponential(scale=1/lambda_, size=len(num_arrivals)), 2)
+        arrival_times = list(np.round(np.cumsum(interarrival_times), 2))
+
+        for numWF in num_arrivals:
+            arrival_time = arrival_times.pop(0)
+            for _ in range(numWF):
+                tasks.append(
+                    [
+                        f't{0}', # TaskName (invalid)
+                        arrival_time, # GenerationTime
+                        0,  # TaskID (invalid)
+                        np.random.randint(*param_TaskSize),  # TaskSize
+                        np.random.randint(*param_CyclesPerBit),  # CyclesPerBit
+                        10 * np.random.randint(*param_TransBitRate),  # TransBitRate
+                        np.random.randint(*param_DDL),  # DDL
+                        f'n{node_id}',  # SrcName
+                    ]
+                )
     ordered_tasks = sorted(tasks, key=lambda x: x[1])
     for i, item in enumerate(ordered_tasks):
         item[0] = f't{i}'  # TaskName (valid)
         item[2] = i  # TaskID (valid)
 
-    # 4. saving
-    if not os.path.exists(save_as):
-        data = pd.DataFrame(ordered_tasks, columns=header)
-        data.to_csv(save_as, index=False)
-    else:
-        print(f"File {save_as} already exists!")
+    return ordered_tasks
 
-    params = {
-        'num_arrivals_per_ingress': num_arrivals_per_ingress,
-        'lambdas': lambdas,
-        'ingress': ingress_line,
-        'param_TaskSize': param_TaskSize,
-        'param_CyclesPerBit': param_CyclesPerBit,
-        'param_TransBitRate': param_TransBitRate,
-        'param_DDL': param_DDL,
+def save_tasks_to_csv(tasks, save_as, header):
+    """
+    Save the generated tasks to a CSV file.
+    
+    :param tasks: List of generated tasks
+    :param save_as: Path to save the CSV file
+    :param header: Header for the CSV file
+    """
+    data = pd.DataFrame(tasks, columns=header)
+    data.to_csv(save_as, index=False)
+    # if not os.path.exists(save_as):
+    #     data = pd.DataFrame(tasks, columns=header)
+    #     data.to_csv(save_as, index=False)
+    # else:
+    #     print(f"File {save_as} already exists!")
+
+def save_params_to_json(params, params_save_as):
+    """
+    Save the parameters to a JSON file.
+    
+    :param params: Dictionary of parameters
+    :param params_save_as: Path to save the JSON file
+    """
+    with open(params_save_as, 'w+') as fw:
+        json.dump(params, fw, indent=4)
+    # if not os.path.exists(params_save_as):
+    #     with open(params_save_as, 'w+') as fw:
+    #         json.dump(params, fw, indent=4)
+    # else:
+    #     print(f"File {params_save_as} already exists!")
+
+
+
+
+def main():
+    """
+    Main function to generate and save synthetic tasks.
+    """
+
+    # 1. parameters
+    header = ['TaskName', 'GenerationTime', 'TaskID', 'TaskSize', 'CyclesPerBit', 
+              'TransBitRate', 'DDL', 'SrcName']  # field names
+    param_TaskSize = (10, 100 + 1)  # Mb
+    param_CyclesPerBit = (100, 1000 + 1)  # per-MBit
+    param_TransBitRate = (1, 5)  # Mbps
+    param_DDL = (20, 100 + 1)  # s
+    param_Latency = (0, 0.005)  # s
+
+    # 2. scenarios
+    scenarios = ['25N50E', '50N50E', '100N150E', 'MilanCityCenter']
+    scenarios_dict_train = {
+        '25N50E': {'nodes': 25, 
+                   'edges': 50, 
+                   'lambda_': np.round(0.04*6.25,  2),
+                   'num_arrivals_per_ingress': 100, 
+                   'ingress_line': list(range(25))},
+        '50N50E': {'nodes': 50,
+                     'edges': 50,
+                     'lambda_': np.round(0.02*6.25, 2),
+                     'num_arrivals_per_ingress': 200,
+                     'ingress_line': list(range(50))},
+        '100N150E': {'nodes': 100,
+                    'edges': 150,
+                    'lambda_': np.round(0.01*6.25, 2),
+                    'num_arrivals_per_ingress': 400,
+                    'ingress_line': list(range(100))},
+        'MilanCityCenter': {'nodes': 30,
+                            'edges': 35,
+                            'lambda_': np.round(0.03333*6.25, 2),
+                            'num_arrivals_per_ingress': 120,
+                            'ingress_line': list(range(30))}
     }
-    json_object = json.dumps(params, indent=4)
-    if not os.path.exists(params_save_as):
-        with open(params_save_as, 'w+') as fw:
-            fw.write(json_object)
-    else:
-        print(f"File {params_save_as} already exists!")
 
-    # 5. loading
-    data = pd.read_csv(save_as)
-    ordered_tasks = list(data.iloc[:].values)
+    scenarios_dict_test = { 
+        '25N50E': {'nodes': 25,
+                   'edges': 50,
+                   'lambda_': np.round(0.04*6.25,2),
+                   'num_arrivals_per_ingress': int(100/2),
+                   'ingress_line': list(range(25))},
+        '50N50E': {'nodes': 50,
+                   'edges': 50,
+                   'lambda_': 0.02*6.25,
+                   'num_arrivals_per_ingress': int(200/2),
+                   'ingress_line': list(range(50))},
+        '100N150E': {'nodes': 100,
+                     'edges': 150,
+                     'lambda_': np.round(0.01*6.25,2),
+                     'num_arrivals_per_ingress': int(400/2),
+                     'ingress_line': list(range(100))},
+        'MilanCityCenter': {'nodes': 30,
+                           'edges': 35,
+                           'lambda_': np.round(0.03333*6.25,2),
+                           'num_arrivals_per_ingress': int(120/2),
+                           'ingress_line': list(range(30))}
+    }
+
+
+    # 3. generate train data
+    for flag, params in scenarios_dict_train.items():
+        num_arrivals_per_ingress = params['num_arrivals_per_ingress']
+        lambda_ = params['lambda_']
+        ingress_line = params['ingress_line']     
+
+        save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/trainset.csv"      
+        params_save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/trainset_configs.json"
+
+        # 3.1. synthetic tasks
+        tasks = generate_tasks(num_arrivals_per_ingress, lambda_, ingress_line, param_TaskSize, param_CyclesPerBit, param_TransBitRate, param_DDL)
+
+        # 3.2. saving
+        save_tasks_to_csv(tasks, save_as, header)
+        params = {
+            'num_arrivals_per_ingress': num_arrivals_per_ingress,
+            'lambdas': [lambda_] * len(ingress_line),
+            'ingress': ingress_line,
+            'param_TaskSize': param_TaskSize,
+            'param_CyclesPerBit': param_CyclesPerBit,
+            'param_TransBitRate': param_TransBitRate,
+            'param_DDL': param_DDL,
+        }
+        save_params_to_json(params, params_save_as)
+
+    # 4. generate test date
+    for flag, params in scenarios_dict_test.items():
+        num_arrivals_per_ingress = params['num_arrivals_per_ingress']
+        lambda_ = params['lambda_']
+
+        with open(f"eval/benchmarks/Topo4MEC/source/{flag}/ingress.txt", 'r') as f:
+            ingress_line = f.readlines()[1].split()
+            ingress_line = [int(item) - 1 for item in ingress_line]  # RayCloudSim is 0-index
+
+        save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/testset.csv"
+        params_save_as = f"eval/benchmarks/Topo4MEC/data/{flag}/testset_configs.json"
+        
+        # 4.1. synthetic tasks
+        tasks = generate_tasks(num_arrivals_per_ingress, lambda_, ingress_line, param_TaskSize, param_CyclesPerBit, param_TransBitRate, param_DDL)
+        
+        # 4.2. saving
+        save_tasks_to_csv(tasks, save_as, header)
+        params = {
+            'num_arrivals_per_ingress': num_arrivals_per_ingress,
+            'lambdas': [lambda_] * len(ingress_line),
+            'ingress': ingress_line,
+            'param_TaskSize': param_TaskSize,
+            'param_CyclesPerBit': param_CyclesPerBit,
+            'param_TransBitRate': param_TransBitRate,
+            'param_DDL': param_DDL,
+        }
+        save_params_to_json(params, params_save_as)
+    # 5. print
+    print("===============================================")
+    print("All tasks generated and saved successfully.")
+    # # 6. loading
+    # data = pd.read_csv(save_as)
+    # ordered_tasks = list(data.iloc[:].values)
+
+
+
 
 
 if __name__ == '__main__':
+    random_seed = 985689
+    np.random.seed(random_seed)
     main()
